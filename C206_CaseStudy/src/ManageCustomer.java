61c479777ef152adcc92f130cf1b8d6eaecf4300
Wayne <20016847@myrp.edu.sg>
2021-08-08 23:44:45 +0800

    Updated manage customer

--- C206_CaseStudy/src/ManageCustomer.java
+++ C206_CaseStudy/src/ManageCustomer.java
@@ -1,22 +1,16 @@
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-
 public class ManageCustomer {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	}
 
-}
 private void registerCustomer() {
 	String name = Helper.readString("Enter name > ");
-	String nric = Helper.readString("Enter NRIC > ");
 	int mobileNumber = Helper.readInt("Enter mobile number > ");
 
 	//TODO: - Write the code to add a new participant.
-	String insertsql= "INSERT INTO customer (name, nric, mobile_number) VALUES('"+ name + "','"+ nric + "'," + mobileNumber + ")";
+	String insertsql= "INSERT INTO customer (name,mobile_number) VALUES('"+ name + "' + mobileNumber + ")";
 	int rowsAdded = DBUtil.execSQL(insertsql);
 	
 	if (rowsAdded == 1) {
@@ -29,34 +23,35 @@ private void registerCustomer() {
 }
 
 //view
-private void findCustomer() {
+	private Customer findCustomer(String mobileNumber) {
 
-	String sql = "SELECT name, mobile_number FROM Customer WHERE nric = '" + nric + "'";
-	Customer p = null;
+		String sql = "SELECT name, mobile_number FROM Customer ";
+		Customer p = null;
 
-	ResultSet rs = DBUtil.getTable(sql);
+		ResultSet rs = DBUtil.getTable(sql);
 
-	try {
-		if (rs.next()) {
-			p = new Customer(nric, rs.getString("name"), rs.getInt("mobile_number"));
-		} else {
-			char confirmRemove = Helper.readChar("Are you sure you wish to remove " + customer.getName() + " (Y/N) > ");
-			if (confirmRemove == 'Y' | confirmRemove == 'y') {
-				String deleteSQL = "DELETE FROM customer WHERE nric='" + nric + "'";
-				int rowsDeleted = DBUtil.execSQL(deleteSQL);
-				if (rowsDeleted == 1) {
-					System.out.println("Customer removed!");
-				} else {
-					System.out.println("Removal failed!");
-				}
+		try {
+			if (rs.next()) {
+				p = new Customer(rs.getString("name"), rs.getInt("mobile_number"));
+			}
+		} catch (SQLException e) {
+			System.out.println("SQL Error: " + e.getMessage());
+		}
+		return p;
+	}
 
+//TODO: - Write the code to delete a customer
+	{
+		char confirmRemove = Helper.readChar("Are you sure you wish to remove " + customer.getName() + " (Y/N) > ");
+		if (confirmRemove == 'Y' | confirmRemove == 'y') {
+			String deleteSQL = "DELETE FROM customer WHERE mobile_number='" + mobileNumber + "'";
+			int rowsDeleted = DBUtil.execSQL(deleteSQL);
+			if (rowsDeleted == 1) {
+				System.out.println("Customer removed!");
+			} else {
+				System.out.println("Removal failed!");
 			}
 
 		}
-	} catch (SQLException e) {
-		System.out.println("SQL Error: " + e.getMessage());
 	}
-	return p;
-
-}
 }
